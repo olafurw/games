5596d00fc1c834d5436dd6890ac4397fe4d65d52
Olafur Waage <olafurw@gmail.com>
2026-06-26 19:47:05 +0200

    solitaire, hopefully this fixes the ios gesture swipe issue

--- solitaire/solitaire_game.9b745c6ecfc36004.js
+++ solitaire/solitaire_game.9b745c6ecfc36004.js
@@ -71,7 +71,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\olafu\AppData\Local\Temp\tmpt8bvf1kw.js
+// include: C:\Users\olafu\AppData\Local\Temp\tmppnsv543u.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -206,21 +206,21 @@ Module['FS_createPath']("/assets", "fonts", true, true);
 
   })();
 
-// end include: C:\Users\olafu\AppData\Local\Temp\tmpt8bvf1kw.js
-// include: C:\Users\olafu\AppData\Local\Temp\tmpriq6u7b_.js
+// end include: C:\Users\olafu\AppData\Local\Temp\tmppnsv543u.js
+// include: C:\Users\olafu\AppData\Local\Temp\tmptwsqgibe.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\olafu\AppData\Local\Temp\tmpriq6u7b_.js
-// include: C:\Users\olafu\AppData\Local\Temp\tmpkppzdsvi.js
+  // end include: C:\Users\olafu\AppData\Local\Temp\tmptwsqgibe.js
+// include: C:\Users\olafu\AppData\Local\Temp\tmpc_26hvbf.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\olafu\AppData\Local\Temp\tmpkppzdsvi.js
+  // end include: C:\Users\olafu\AppData\Local\Temp\tmpc_26hvbf.js
 
 
 var arguments_ = [];
